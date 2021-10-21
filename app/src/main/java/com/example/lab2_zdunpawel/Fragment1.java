081c83f12a54d64a12392c6f472f61412974535f
pawzdu <pawel.karate@wp.pl>
2021-10-21 16:58:17 +0200

    Lab3

--- app/src/main/java/com/example/lab2_zdunpawel/Fragment1.java
+++ app/src/main/java/com/example/lab2_zdunpawel/Fragment1.java
@@ -1,5 +1,5 @@
 package com.example.lab2_zdunpawel;
-
+/*
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  * Use the {@link Fragment1#newInstance} factory method to
  * create an instance of this fragment.
- */
+
 public class Fragment1 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -35,7 +35,7 @@ public class Fragment1 extends Fragment {
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment Fragment1.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static Fragment1 newInstance(String param1, String param2) {
         Fragment1 fragment = new Fragment1();
@@ -60,5 +60,68 @@ public class Fragment1 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+}*/
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class Fragment1 extends Fragment {
+    public interface OnButtonClickListener {
+        public void onButtonClickShuffle();
+        public void onButtonClickClockwise();
+        public void onButtonClickHide();
+        public void onButtonClickRestore();
+    }
+    //2.
+    private OnButtonClickListener callback = null;
+    //3.
+    public void setOnButtonClickListener(OnButtonClickListener callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        Button buttonShuffle = (Button) view.findViewById(R.id.button_shuffle);
+        Button buttonClockwise = (Button) view.findViewById(R.id.button_clockwise);
+        Button buttonHide = (Button) view.findViewById(R.id.button_hide);
+        Button buttonRestore = (Button) view.findViewById(R.id.button_restore);
+        buttonShuffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) callback.onButtonClickShuffle();
+            }
+        });
+        buttonClockwise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) callback.onButtonClickClockwise();
+            }
+        });
+        buttonHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) callback.onButtonClickHide();
+            }
+        });
+        buttonRestore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) callback.onButtonClickRestore();
+            }
+        });
+        return view;
     }
 }
